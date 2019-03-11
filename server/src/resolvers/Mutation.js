2e64c39af4acc9d3459a9ed299ba220422331df7
thevenice <pp886655@gmail.com>
2019-03-11 13:48:31 +0530

    returned/Customtype/mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,10 +1,15 @@
+const { titles } = require("./Query");
+let idCount = titles.length;
 const Mutation = {
-  post: (_, args, context, info) =>
-    `${args.description} and
-    ${args.createdBy}
-    ${args.location}
-
-    `
+  post: (_, args, context, info) => {
+    const title = {
+      id: `link-${idCount++}`,
+      title: args.title,
+      location: args.location
+    };
+    titles.push(title);
+    return title;
+  }
 };
 
 module.exports = { Mutation };
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -10,4 +10,4 @@ const Query = {
   feed: () => titles
 };
 
-module.exports = { Query };
+module.exports = { Query, titles };
